ffe4dddce95013a4951f022e3dc079b61ef695c0
blaudev <danielpeiro72@gmail.com>
2024-05-08 19:18:58 +0200

    Skill groups

--- src/app/components/skill-group/skill-group.component.ts
+++ src/app/components/skill-group/skill-group.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Skill } from '../../models/Skill';
 
 @Component({
   selector: 'app-skill-group',
   standalone: true,
   imports: [],
   templateUrl: './skill-group.component.html',
-  styleUrl: './skill-group.component.css'
+  styleUrl: './skill-group.component.css',
 })
 export class SkillGroupComponent {
+  @Input() name: string = 'Skill Group 1';
+  @Input() groups: number = 5;
+  @Input() levelPerGroup: number = 5;
+  @Input() skillsColumnWidth: string = '250px';
+  @Input() cellheight: string = '50px';
+  @Input() skills: Skill[] = [];
 
+  groupStyle() {
+    return {
+      'grid-template-columns': `${this.skillsColumnWidth} repeat(${
+        this.groups * this.levelPerGroup
+      }, 1fr)`,
+    };
+  }
+
+  cells() {
+    return Array.from(
+      { length: this.groups * this.levelPerGroup },
+      (_, i) => i
+    );
+  }
 }
